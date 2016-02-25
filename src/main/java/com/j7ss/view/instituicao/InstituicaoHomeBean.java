b45dafcbf93ed032bfca2e7d9be87c0cd4cc217a
Edivando <edivando7@gmail.com>
2016-02-25 09:53:23 -0300

    Test send email

--- src/main/java/com/j7ss/view/instituicao/InstituicaoHomeBean.java
+++ src/main/java/com/j7ss/view/instituicao/InstituicaoHomeBean.java
@@ -8,11 +8,14 @@
  */
 package com.j7ss.view.instituicao;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 import com.j7ss.entity.DocumentoVagaEstagio;
 import com.j7ss.util.BasicView;
+import com.j7ss.util.email.MailApi;
+import com.j7ss.util.email.MailApiException;
 
 /**
  * 
@@ -26,49 +29,15 @@ public class InstituicaoHomeBean extends BasicView<DocumentoVagaEstagio> {
 	
 	private static final long serialVersionUID = 1L;
 	
-//	@Setter
-//	@ManagedProperty(value="#{loginBean}")
-//	private LoginBean loginBean;
-//	
-//	@Setter
-//	private DocumentoVagaEstagioMessage docMessage;
-//	
-//	@Override
-//	public List<DocumentoVagaEstagio> getEntitys() {
-//		return entitys == null ? entitys = DocumentoVagaEstagio.findByDocumentoStatus(DocumentoStatus.AGUARDANDO_VERIFICACAO) : entitys;
-//	}
-//	
-//	public String getDocPage(){
-//		return new DocumentoParse(getEntity()).toPage();
-//	}
-//	
-//	public void saveErro(){
-//		try {
-//			entity.status(DocumentoStatus.VERIFICADO_COM_ERRO).save();
-//			getDocMessage().date(new Date()).usuario(loginBean.getUsuario()).documentoVagaEstagio(entity).save();
-//			entity.save();
-//		} catch (DAOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		grid();
-//	}
-//	
-//	public void saveDownload(){
-//		try {
-//			entity.status(DocumentoStatus.DISPONIVEL_DOWNLOAD).save();
-//			getDocMessage().date(new Date()).usuario(loginBean.getUsuario()).documentoVagaEstagio(entity).save();
-//			entity.save();
-//		} catch (DAOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		grid();
-//	}
-//	
-//	public DocumentoVagaEstagioMessage getDocMessage() {
-//		return docMessage == null ? docMessage = new DocumentoVagaEstagioMessage() : docMessage;
-//	}
+	@PostConstruct
+	public void init(){
+		try {
+			new MailApi().to("edivando7@gmail.com", "Edivando Alves").subject("titulo").content("message").send();
+		} catch (MailApiException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
 
 }
